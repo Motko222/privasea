6fa52db44fe7f2dadbb14384ee45fdd1c9315b46
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-02 22:43:26 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,5 +5,7 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 
 [ -f config ] || cp config.sample config
-nano config
 
+source config
+[ -d /root/hyperlane_db_$CHAIN ] || mkdir -p /root/hyperlane_db_$CHAIN && chmod -R 777 /root/hyperlane_db_$CHAIN
+nano config
