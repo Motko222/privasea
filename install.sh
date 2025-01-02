9c567e31b063d2068e5060c891fae2a8889c2e16
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-02 22:27:18 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+source config
+
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install docker.io -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 20
+
+npm install -g @hyperlane-xyz/cli
+docker pull --platform linux/amd64 gcr.io/abacus-labs-dev/hyperlane-agent:agents-v1.0.0
+mkdir -p /root/hyperlane_db_$CHAIN && chmod -R 777 /root/hyperlane_db_$CHAIN
