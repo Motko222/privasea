e92e25527973c5fcb4c7a3442a045f1925a8439c
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-31 21:43:06 +0100

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -4,8 +4,6 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
 echo "Stopping containers..."
-container=$(docker ps | grep $folder | awk '{print $NF}')
-docker stop $container
+docker stop $folder
 echo "Removing containers..."
-container=$(docker ps -a | grep $folder | awk '{print $NF}')
-docker rm $container 
+docker rm $folder
