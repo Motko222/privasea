d7530eacf7a91f153f173cb8007c240344658de6
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-31 21:41:41 +0100

    Update create-key.sh

--- create-key.sh
+++ create-key.sh
@@ -3,10 +3,12 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
+version=v0.0.1
+
 apt-get install docker.io -y
-docker pull privasea/node-client:$CLIENT_VERSION
+docker pull privasea/node-client:$version
 mkdir -p /root/privasea-keys
-docker run -it -v /root/privasea-keys:/app/keys privasea/node-client:$CLIENT_VERSION account
+docker run -it -v /root/privasea-keys:/app/keys privasea/node-client:$version account
 key_file=$(ls /root/privasea-keys)
 
 echo ----- BACKUP THIS KEY !!! -------------------
