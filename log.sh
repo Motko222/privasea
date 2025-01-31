302ced1d72162d2cdb30b46d3de80a620eb4f3fc
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-31 21:42:33 +0100

    Update log.sh

--- log.sh
+++ log.sh
@@ -3,5 +3,4 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-container=$(docker ps -a | grep $folder | awk '{print $NF}')
-docker logs -n 200 -f $container
+docker logs -n 200 -f $folder
